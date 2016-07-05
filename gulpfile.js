d827e29fd8f23e486d9796175bce045afe43f3f3
ridomin <ridomin@live.com>
2016-07-05 15:39:14 -0700

    adding gulp to build TypeScript

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+﻿/// <binding BeforeBuild='default' />
+var gulp = require("gulp");
+var tsc = require("gulp-typescript");
+
+gulp.task("default", function () {
+    var tsProject = tsc.createProject("tsconfig.json");
+    return tsProject.src().pipe(tsc(tsProject));
+});
+
